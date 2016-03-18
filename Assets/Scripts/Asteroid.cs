b04c746bbe70bd55d158214638f94cf6c96fbb92
Nick Conlon <ncon67@gmail.com>
2016-03-18 02:53:19 -0400

    Cleaned up code and assets from Space. Kinda playable in the scene view

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Asteroid : MonoBehaviour
+{
+	public const float GRAVITATION_MAGNITUDE = 100.0f;
+
+	public Vector3 centerOfOrbit;
+
+	private Rigidbody2D myRigidbody;
+
+	void Start()
+	{
+		myRigidbody = GetComponent<Rigidbody2D>();
+
+		centerOfOrbit = new Vector3( 0.0f, 0.0f, 0.0f );
+
+		Vector3 toCenter = centerOfOrbit - transform.position;
+		Vector2 tangential = new Vector2( -toCenter.y, toCenter.x );
+		tangential.Normalize();
+
+		Vector3 initForce = new Vector3( tangential.x, tangential.y, 0.0f );
+		initForce *= Mathf.Sqrt( GRAVITATION_MAGNITUDE * ( 100.0f / toCenter.magnitude - 1.0f / toCenter.magnitude ) );
+
+		myRigidbody.AddForce( initForce, ForceMode2D.Impulse );
+	}
+
+	void FixedUpdate()
+	{
+		CalculateOrbitalForce ();
+	}
+
+	private void CalculateOrbitalForce()
+	{
+		Vector3 toCenter = centerOfOrbit - transform.position;
+		float radius = toCenter.magnitude;
+		toCenter.Normalize();
+		Vector3 gravity = GRAVITATION_MAGNITUDE * toCenter / (radius * radius);
+		myRigidbody.AddForce( gravity );
+	}
+}
--- Assets/Scripts/BlackHole.cs
+++ Assets/Scripts/BlackHole.cs
@@ -3,66 +3,44 @@ using System.Collections;
 
 public class BlackHole : MonoBehaviour
 {
-	public float rotationSpeed = 200;
-	public float suckDelay = 1.0f;
-	public float speed = 10.0f;
+	public float suckForce = 5.0f;
+	public float rotationSpeed = 200.0f;
 	public float radius = 10.0f;
 
-	private Rigidbody2D m_rigidbody;
-	private ParticleSystem m_particleSys;
-	private bool m_sucking = false;
+	private ParticleSystem particleSys;
 
 	void Start()
 	{
-		m_rigidbody = GetComponent<Rigidbody2D>();
-		m_particleSys = GetComponent<ParticleSystem>();
+		particleSys = GetComponent<ParticleSystem>();
 
-		m_particleSys.startSpeed = -radius;
+		particleSys.startSpeed = -radius;
 		transform.localScale *= radius / 10;
-	}
 
-	void Fire( Vector2 inheritedVelocity )
-	{
-		m_rigidbody.velocity = (Vector2)transform.up * speed + inheritedVelocity;
+		particleSys.Play();
 	}
 
 	void Update()
 	{
-		transform.Rotate( 0, 0, rotationSpeed * Time.deltaTime );
-
-		if( !m_sucking )
-		{
-			suckDelay -= Time.deltaTime;
-			
-			if( suckDelay <= 0 )
-			{
-				m_sucking = true;
-				Destroy( m_rigidbody );
-				m_particleSys.Play();
-			}
-		}
+		//transform.Rotate( 0, 0, rotationSpeed * Time.deltaTime );
 	}
 
 	void FixedUpdate()
 	{
-		if( m_sucking )
+		Collider2D[] hitColliders = Physics2D.OverlapCircleAll( transform.position, radius );
+
+		foreach( Collider2D col in hitColliders )
 		{
-			Collider2D[] hitColliders = Physics2D.OverlapCircleAll( transform.position, radius );
+			Rigidbody2D rb = col.GetComponent<Rigidbody2D>();
 			
-			foreach( Collider2D col in hitColliders )
+			if( rb != null )
 			{
-				Rigidbody2D rb = col.GetComponent<Rigidbody2D>();
-				
-				if( rb != null )
-				{
-					Vector3 toCenter =  transform.position - col.transform.position;
+				Vector3 toCenter =  transform.position - col.transform.position;
 
-					float percent = Mathf.Max( 0,  radius - toCenter.magnitude ) / radius;
+				float percent = Mathf.Max( 0,  radius - toCenter.magnitude ) / radius;
 
-					//Vector3 tangential = new Vector3( -toCenter.y, toCenter.x );
-					Vector3 force = toCenter * ( 1 - percent ) * 10 * rb.mass;
-					rb.AddForce( force, ForceMode2D.Force );
-				}
+				//Vector3 tangential = new Vector3( -toCenter.y, toCenter.x );
+				Vector3 force = toCenter * ( 1 - percent ) * suckForce;// * rb.mass;
+				rb.AddForce( force, ForceMode2D.Force );
 			}
 		}
 	}
