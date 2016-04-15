a2c513381af06934f1c83fdbc3f8bbfdde37b5c6
JosephCoppola <jjc9718@rit.edu>
2016-04-15 00:35:39 -0400

    Pause Screen Added

--- Assets/Scripts/UI/PausePanelControl.cs
+++ Assets/Scripts/UI/PausePanelControl.cs
@@ -1,15 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PausePanelControl : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	[SerializeField]
+	private Text pauseScoreText;
+	[SerializeField]
+	private Animator panelAnimator;
+	public Animator PanelAnimator 
+	{
+		get { return panelAnimator; }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void UpdateScore(int score)
+	{
+		pauseScoreText.text = score.ToString ();
+	}
+
+	public void DisableLossScreenObject() 
+	{
+		gameObject.transform.parent.gameObject.SetActive (false);
+	}
+
+	public void ClosePauseScreen()
+	{
+		Time.timeScale = 1.0f;
+		panelAnimator.SetTrigger ("Close");
 	}
 }
--- Assets/Scripts/UI/SceneControllers/GameUIController.cs
+++ Assets/Scripts/UI/SceneControllers/GameUIController.cs
@@ -13,6 +13,8 @@ public class GameUIController : MonoBehaviour {
 	[SerializeField]
 	private LossPanelController lossPanel;
 	[SerializeField]
+	private PausePanelControl pausePanel;
+	[SerializeField]
 	private Text scoreText;
 
 
@@ -33,7 +35,14 @@ public class GameUIController : MonoBehaviour {
 
 	public void TriggerDeathUI() 
 	{
-		lossPanel.gameObject.SetActive (true);
+		Debug.Log ("Death");
+		lossPanel.transform.parent.gameObject.SetActive (true);
 		lossPanel.UpdateScore (playerBubble.GetScore ());
+	}
+
+	public void Pause() 
+	{
+		Time.timeScale = 0.0f;
+		pausePanel.transform.parent.gameObject.SetActive (true);	
 	}
 }
